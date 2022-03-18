b70d0cf0d1992417ff764e02a9800b3e6a3a8b59
DeltaUpsilonDu <subscr1pt10n+g1t@myfr33dom.net>
2022-03-18 11:44:54 +0100

    Adding of my first smart contracts, exercices of Solidity chapter.

---
+++ 3. Solidity/Whitelist.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.9;
+ 
+contract Whitelist {
+  struct Person { // Structure de données
+      string name;
+      uint age;  
+  }
+ 
+  function addPerson(string memory _name, uint _age) public {
+      Person memory person;
+      person.name = _name;
+      person.age = _age;
+  }
+}
