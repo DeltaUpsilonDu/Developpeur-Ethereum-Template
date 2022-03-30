4d2803d2efa1f6ed613394d4c362a06398c26c98
DeltaUpsilonDu <subscr1pt10n+g1t@myfr33dom.net>
2022-03-30 14:03:14 +0200

    Modification of the erc20.test0js file for the exercice TDD - ERC20.

--- 4. Truffle & CI-CD/TDD ERC20/test/erc20.test.js
+++ 4. Truffle & CI-CD/TDD ERC20/test/erc20.test.js
@@ -1,5 +1,6 @@
 // erc20.test.js 
-const { BN, ether } = require('@openzeppelin/test-helpers');
+const { BN, ether, expectRevert } = require('@openzeppelin/test-helpers');
+const { ZERO_ADDRESS } = require('@openzeppelin/test-helpers/src/constants');
 const { expect } = require('chai');
 const ERC20 = artifacts.require('ERC20Token');
 contract('ERC20', function (accounts) {
@@ -9,6 +10,7 @@ const _initialsupply = new BN(1000);
 const _decimals = new BN(18);
 const owner = accounts[0];
 const recipient = accounts[1];
+const spender = accounts[2];
  
  beforeEach(async function () {
   this.ERC20Instance = await ERC20.new(_initialsupply,{from: owner});
@@ -40,4 +42,39 @@ it('vérifie si un transfer est bien effectué', async function (){
   expect(balanceOwnerAfterTransfer).to.be.bignumber.equal(balanceOwnerBeforeTransfer.sub(amount));
   expect(balanceRecipientAfterTransfer).to.be.bignumber.equal(balanceRecipientBeforeTransfer.add(amount));
 });
+/*
+it('vérifie que l addresse zéro n est pas acceptée', async function (){
+    let amount = new BN(10);
+
+    //expect(await this.ERC20Instance.approve(ZERO_ADDRESS,amount)).to.be.false;
+
+    await expectRevert(this.ERC20Instance.approve(ZERO_ADDRESS,amount,{from: owner}),"ERC20: approve from the zero address");
+} );
+*/
+it('vérifie si un approve est bien effectué', async function (){
+    let allowanceSpenderBeforeApprove = await this.ERC20Instance.allowance(owner, spender);
+    let amount = new BN(10);
+  
+    await this.ERC20Instance.approve(spender, amount, {from: owner});
+  
+    let allowanceSpenderAfterApprove = await this.ERC20Instance.allowance(owner, spender);
+   
+    expect(allowanceSpenderAfterApprove).to.be.bignumber.equal(allowanceSpenderBeforeApprove.add(amount));
+  });
+  
+  it('vérifie si un transfer from est bien effectué', async function (){
+    let balanceOwnerBeforeTransfer = await this.ERC20Instance.balanceOf(owner);
+    let balanceRecipientBeforeTransfer = await this.ERC20Instance.balanceOf(recipient);
+    let amount = new BN(10);
+  
+    await this.ERC20Instance.approve(spender, amount, {from: owner});
+    await this.ERC20Instance.transferFrom(owner, recipient, amount, {from: spender});
+  
+    let balanceOwnerAfterTransfer = await this.ERC20Instance.balanceOf(owner);
+    let balanceRecipientAfterTransfer = await this.ERC20Instance.balanceOf(recipient);
+   
+    expect(balanceOwnerAfterTransfer).to.be.bignumber.equal(balanceOwnerBeforeTransfer.sub(amount));
+    expect(balanceRecipientAfterTransfer).to.be.bignumber.equal(balanceRecipientBeforeTransfer.add(amount));
+  });
+
 });
