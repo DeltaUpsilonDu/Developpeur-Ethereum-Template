7292d39f720d1313d0fee885b6d93cf96c2097ca
DeltaUpsilonDu <subscr1pt10n+g1t@myfr33dom.net>
2022-04-26 17:00:59 +0200

    Adding of all the needed files for the DApp project.

--- 6. Dapp/Projet #3/client/src/App.js
+++ 6. Dapp/Projet #3/client/src/App.js
@@ -1,11 +1,22 @@
+// Import libraries
 import React, { Component } from "react";
-import SimpleStorageContract from "./contracts/SimpleStorage.json";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Card from 'react-bootstrap/Card';
+
+
+// Import contract
+import VotingContract from "./contracts/Voting.json";
 import getWeb3 from "./getWeb3";
+
+// Import components
+import AdminDashboard from "./Components/AdminDashboard.js";
+import VoterDashboard from "./Components/VoterDashboard.js";
+import WinningVote from "./Components/WinningVote.js";
 
 import "./App.css";
 
 class App extends Component {
-  state = { storageValue: 0, web3: null, accounts: null, contract: null };
+  state = { owned:false, registered:false, hasVoted:false, voteID:0, workFlowStatus:0, whitelist: null, proposals: null, winningVote: null, listEventsVoted: null, web3: null, accounts: null, contract: null };
 
   componentDidMount = async () => {
     try {
@@ -17,15 +28,59 @@ class App extends Component {
 
       // Get the contract instance.
       const networkId = await web3.eth.net.getId();
-      const deployedNetwork = SimpleStorageContract.networks[networkId];
+      const deployedNetwork = VotingContract.networks[networkId];
+
       const instance = new web3.eth.Contract(
-        SimpleStorageContract.abi,
+        VotingContract.abi,
         deployedNetwork && deployedNetwork.address,
       );
 
-      // Set web3, accounts, and contract to the state, and then proceed with an
-      // example of interacting with the contract's methods.
-      this.setState({ web3, accounts, contract: instance }, this.runExample);
+      // Get the contract owner address
+      const owner = await instance.methods.owner().call();
+      console.log(owner);
+      let owned = accounts[0]==owner;
+      console.log(owned);
+
+      // Get the voter object
+      let voter = await instance.methods.getVoter(accounts[0]).call({from : owner});
+      let registered = voter[0];
+      let hasVoted = voter[1];
+      let voteID = voter[2];
+      console.log(registered);
+      console.log(hasVoted);
+      console.log(voteID);
+
+      const workFlowStatus = await instance.methods.workflowStatus().call();
+      console.log(workFlowStatus);
+
+      // Get the whitelist and the proposals
+      let whitelist;
+      let proposals = null;
+      if(registered) 
+        {
+          whitelist= await instance.methods.getWhiteList().call({from : accounts[0]});
+          if(workFlowStatus >= 1) {proposals= await instance.methods.getProposals().call({from : accounts[0]});}
+        }
+      
+      // Get the winning vote
+      let winningVote;
+      if(workFlowStatus == 5) {winningVote = await instance.methods.getWinner().call({from : accounts[0]});}
+      console.log(winningVote);
+
+      // Setup the options and get the events
+      let options = {
+        fromBlock: 0,              
+        toBlock: 'latest'
+      };
+      const listEventsVoted = await instance.getPastEvents('Voted', options);
+
+      // Set web3, accounts, and contract to the state.
+      this.setState({ owned, registered, hasVoted, voteID, workFlowStatus, whitelist, proposals, winningVote, listEventsVoted, web3, accounts, contract: instance });
+
+      window.ethereum.on('accountsChanged', () => {
+        window.location.reload();
+      })
+
     } catch (error) {
       // Catch any errors for any of the above operations.
       alert(
@@ -35,36 +90,96 @@ class App extends Component {
     }
   };
 
-  runExample = async () => {
-    const { accounts, contract } = this.state;
-
-    // Stores a given value, 5 by default.
-    await contract.methods.set(5).send({ from: accounts[0] });
-
-    // Get the value from the contract to prove it worked.
-    const response = await contract.methods.get().call();
-
-    // Update state with the result.
-    this.setState({ storageValue: response });
+  addNewVoter = async (adr) => {  
+    const { accounts, contract } = this.state;  
+    console.log(adr);
+    await contract.methods.addVoter(adr).send({from: accounts[0]});
+    //window.location.reload();
+    const whitelist = await contract.methods.getWhiteList().call({from : accounts[0]});
+    this.setState({ whitelist: whitelist });
   };
 
+  addNewProposal = async (proposal) => {  
+    const { accounts, contract } = this.state;  
+    console.log(proposal);
+    await contract.methods.addProposal(proposal).send({from: accounts[0]});
+    const proposals = await contract.methods.getProposals().call({from : accounts[0]});
+    this.setState({ proposals: proposals });
+  };
+
+  saveVote = async (voteID) => {  
+    const { accounts, contract } = this.state;  
+    console.log(voteID);
+    await contract.methods.setVote(voteID).send({from: accounts[0]});
+    const voter = await contract.methods.getVoter(accounts[0]).call({from : accounts[0]});
+    this.setState({hasVoted:voter[1], voteID:voter[2]});
+  };
+
+  nextStatus= async () => {  
+    const { accounts, contract} = this.state;  
+    if(this.state.workFlowStatus < 5) {
+      await contract.methods.nextStatus().send({from: accounts[0]});
+
+      // Get the winning vote
+      if(this.state.workFlowStatus == 4) {
+        let winningVote;
+        winningVote = await contract.methods.getWinner().call({from : accounts[0]});
+        this.setState({ winningVote: winningVote});
+        console.log("Check winner");
+      }
+     
+    } else {
+      await contract.methods.reset().send({from: accounts[0]});
+      console.log("Reset requested");
+      // Refresh the whitelist and the proposals
+      const whitelist = await contract.methods.getWhiteList().call({from : accounts[0]});
+      const proposals = null;
+      this.setState({whitelist: whitelist ,proposals: proposals});
+      //window.location.reload();
+    }
+    const workFlowStatus = await contract.methods.workflowStatus().call();
+    this.setState({ workFlowStatus: workFlowStatus});
+    console.log(workFlowStatus);
+  };
+ 
   render() {
+    const statusText = ["Voters registration", "Proposals registration", "End of proposal registration", "Voting session", "End of voting session", "Votes tallied"];
     if (!this.state.web3) {
       return <div>Loading Web3, accounts, and contract...</div>;
     }
     return (
       <div className="App">
-        <h1>Good to Go!</h1>
-        <p>Your Truffle Box is installed and ready.</p>
-        <h2>Smart Contract Example</h2>
-        <p>
-          If your contracts compiled and migrated successfully, below will show
-          a stored value of 5 (by default).
-        </p>
-        <p>
-          Try changing the value stored on <strong>line 42</strong> of App.js.
-        </p>
-        <div>The stored value is: {this.state.storageValue}</div>
+        <div className="Account">Connected wallet: {this.state.accounts[0]}</div>
+        <br></br>
+       <div style={{display: 'flex', justifyContent: 'center'}}>
+        <Card style={{ width: '50rem' }}>
+            <Card.Header><h1>VOTING PROJECT #3</h1><p>{statusText[this.state.workFlowStatus]}</p></Card.Header>
+            <Card.Body>
+              <AdminDashboard 
+                own={this.state.owned} 
+                state={this.state.workFlowStatus} 
+                getNewVoter={this.addNewVoter}
+                adminEvent={this.nextStatus}
+              />
+              <br></br>
+              <VoterDashboard 
+                register={this.state.registered} 
+                vote={this.state.voteID}
+                didVote={this.state.hasVoted}
+                state={this.state.workFlowStatus} 
+                list={this.state.whitelist}
+                proposal={this.state.proposals}
+                getNewProposal={this.addNewProposal}
+                getVote={this.saveVote}
+              />
+              <WinningVote 
+                state={this.state.workFlowStatus} 
+                winningVote={this.state.winningVote}
+                eventsVoted={this.state.listEventsVoted} 
+              />
+            </Card.Body>
+          </Card>
+      </div>
       </div>
     );
   }
--- 6. Dapp/Projet #3/contracts/Voting.sol
+++ 6. Dapp/Projet #3/contracts/Voting.sol
@@ -39,8 +39,8 @@ contract Voting is Ownable {
         VotesTallied
     }
 
-    /// @notice Variable to contain the current status
-    WorkflowStatus private _status;
+    /// @notice Variable to contain the current status. V2: changed to public (was private)
+    WorkflowStatus public workflowStatus;
     /// @notice Mapping of voters
     mapping (address => Voter) voters;
     /// @notice Table of voters addresses (used to reset the voting)
@@ -89,19 +89,19 @@ contract Voting is Ownable {
     /// @notice This function allows the owner to manage the voting status
     /// @dev Function to be called by the admin through the front end
     function nextStatus() external onlyOwner {
-        require(_status != WorkflowStatus.VotesTallied, "The voting has already ended.");    // Make sure the voting is not finished
+        require(workflowStatus != WorkflowStatus.VotesTallied, "The voting has already ended.");    // Make sure the voting is not finished
         
-        uint _oldStatusID = uint(_status);           // Save the current status as old status
-        _status = WorkflowStatus(_oldStatusID+1);    // Update the status 
+        uint _oldStatusID = uint(workflowStatus);           // Save the current status as old status
+        workflowStatus = WorkflowStatus(_oldStatusID+1);    // Update the status 
     
-        emit WorkflowStatusChange(WorkflowStatus(_oldStatusID),_status);  // Raise the event to log the status change 
+        emit WorkflowStatusChange(WorkflowStatus(_oldStatusID),workflowStatus);  // Raise the event to log the status change 
     }
 
     /// @notice This function allows the owner to add a voter in the whitelist
     /// @dev Function to be called by the admin through the front end
     /// @param _address The address to be added in the whitelist
     function addVoter(address _address) public onlyOwner {
-        require(_status == WorkflowStatus.RegisteringVoters,"The registering period is over.");      // Make sure the registration phase is still in progress
+        require(workflowStatus == WorkflowStatus.RegisteringVoters,"The registering period is over.");      // Make sure the registration phase is still in progress
         require(_address != address(0),"The address needs to be different from zero.");             // Make sure the address is valid
         require(voters[_address].isRegistered == false,"This voter has been already registered.");   // Revert if the voter was already registered (to aboid unnecessary actions)
 
@@ -117,7 +117,7 @@ contract Voting is Ownable {
     /// @dev Function to be called by the voters through the front end
     /// @param _description The description of the proposal
     function addProposal(string memory _description) external onlyVoters {
-        require(_status == WorkflowStatus.ProposalsRegistrationStarted,"The proposal registering period is over or has not strted yet.");      // Make sure the proposal registration phase is in progress
+        require(workflowStatus == WorkflowStatus.ProposalsRegistrationStarted,"The proposal registering period is over or has not started yet.");      // Make sure the proposal registration phase is in progress
     
         // Push the new proposal in the array
         Proposal memory proposal = Proposal(_description, 0);
@@ -130,7 +130,7 @@ contract Voting is Ownable {
     /// @dev Function to be called by the voters through the front end
     /// @param _proposalID The proposal ID that receive a vote
     function setVote(uint _proposalID) external onlyVoters {
-        require(_status == WorkflowStatus.VotingSessionStarted,"The proposal voting period is over or has not started yet.");      // Make sure the voting phase is in progress
+        require(workflowStatus == WorkflowStatus.VotingSessionStarted,"The proposal voting period is over or has not started yet.");      // Make sure the voting phase is in progress
         require(voters[msg.sender].hasVoted == false,"This voter has already voted.");   // Revert if the voter has already voted
         require(_proposalID <= _proposals.length-1,"The proposal ID is not valid.");
 
@@ -150,7 +150,7 @@ contract Voting is Ownable {
     /// @notice This function used to calculate the voting result. V2: removed in order to avoid a DoS - Gas Limit
     /// @dev Function to be called by the admin through the front end
     /*function calculateResult() external onlyOwner {
-        require(_status == WorkflowStatus.VotingSessionEnded,"The voting is still in progress or has not started yet.");        // Make sure the voting is finished
+        require(workflowStatus == WorkflowStatus.VotingSessionEnded,"The voting is still in progress or has not started yet.");        // Make sure the voting is finished
         require(_proposals.length > 0,"There are no proposals in the list.");                            // Make sure there is at least one proposal in the list
 
         for(uint i=1;i<_proposals.length;i++) {
@@ -164,31 +164,32 @@ contract Voting is Ownable {
     /// @dev Function to be called by anyone through the front end
     /// @return Returns the winning proposal ID, his description and the amount of votes
     function getWinner() external view returns (uint,string memory,uint) {
-        require(_status == WorkflowStatus.VotesTallied,"The voting is still in progress.");      // Make sure the voting is finished
+        require(workflowStatus == WorkflowStatus.VotesTallied,"The voting is still in progress.");      // Make sure the voting is finished
 
         return (_winningProposalId,_proposals[_winningProposalId].description,_proposals[_winningProposalId].voteCount);
     }
 
-    /// @notice This function allows the voters to see what the other voters have voted
+/*
+    /// @notice This function allows the voters to see what the other voters have voted. V2: code optimization, this function is not needed anymore
     /// @dev Function to be called by the voters through the front end
     /// @param _address The address to check the vote
     /// @return Returns the voted ID of the given voter defined by _address
     function getVotedID(address _address) external view onlyVoters returns (uint) {
-        require(_status >= WorkflowStatus.VotingSessionStarted,"The voting period did not start yet.");      // Make sure the voting phase has started
+        require(workflowStatus >= WorkflowStatus.VotingSessionStarted,"The voting period did not start yet.");      // Make sure the voting phase has started
         require(_address != address(0),"The address needs to be different from zero.");             // Make sure the address is valid
         require(voters[_address].isRegistered == true,"This address does not belong to the whitelist.");
         require(voters[_address].hasVoted == true,"This voter has not voted yet.");   // Revert if the voter has not voted yet
     
         return voters[_address].votedProposalId;    // Return the vote of the requested voter
-    }
+    }*/
 
     /// @notice This function resets the voting once closed
     /// @dev Function to be called by the admin through the front end
     function reset() external onlyOwner{
-        require(_status == WorkflowStatus.VotesTallied, "The voting is still ongoing.");    // Make sure the voting is finished
+        require(workflowStatus == WorkflowStatus.VotesTallied, "The voting is still ongoing.");    // Make sure the voting is finished
 
-        _status = WorkflowStatus.RegisteringVoters;  // Reset status
-        emit WorkflowStatusChange(WorkflowStatus(WorkflowStatus.VotesTallied),_status);  // Raise the event to log the status change 
+        workflowStatus = WorkflowStatus.RegisteringVoters;  // Reset status
+        emit WorkflowStatusChange(WorkflowStatus(WorkflowStatus.VotesTallied),workflowStatus);  // Raise the event to log the status change 
 
         _winningProposalId = 0;                      // Reset winning ID
 
@@ -214,7 +215,7 @@ contract Voting is Ownable {
     /// @dev Function to be called by the owner through the front end
     /// @param _address The address to become the new owner
     function changeOwner(address _address) external onlyOwner {
-        require(_status == WorkflowStatus.RegisteringVoters,"The registering period is over.");      // Make sure the registration phase is still in progress
+        require(workflowStatus == WorkflowStatus.RegisteringVoters,"The registering period is over.");      // Make sure the registration phase is still in progress
         require(_address != address(0),"The address needs to be different from zero.");             // Make sure the address is valid
         
         voters[msg.sender].isRegistered == false ;      // Remove the rights to the old owner (the new one can still add him, in case)
@@ -226,4 +227,27 @@ contract Voting is Ownable {
         transferOwnership(_address);                    // Call the function to effectively change the owner
     }
 
+    // ::::::::::::: V2: NEW GETTERS to help with the creation of the DApp::::::::::::: //
+
+    /// @notice This function returns a list of registered voters, only for the voters
+    /// @dev Function to be called by the voters through the front end
+    /// @return Returns a list of addresses
+    function getWhiteList() external onlyVoters view returns(address[] memory){
+       return _whitelist;
+    }
+
+    function getVoter(address _address) external onlyVoters view returns (Voter memory) {
+        require(_address != address(0),"The address needs to be different from zero.");             // Make sure the address is valid
+
+        return voters[_address];
+    }
+
+    /// @notice This function returns a list of registered voters, only for the voters
+    /// @dev Function to be called by the voters through the front end
+    /// @return Returns a list of addresses
+    function getProposals() external onlyVoters view returns(Proposal[] memory){
+        require(workflowStatus >= WorkflowStatus.ProposalsRegistrationStarted,"The proposal registering period has not started yet.");      // Make sure the proposal registration phase has started
+       
+        return _proposals;
+    }
 }
